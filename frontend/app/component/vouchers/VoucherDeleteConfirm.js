19d676777e0801503e15ea6c90add6d71597a4de
Addin Gama Bertaqwa <punyagama@yahoo.com>
2017-01-17 16:21:26 +0700

    Fixing delete voucher

--- frontend/app/component/vouchers/VoucherDeleteConfirm.js
+++ frontend/app/component/vouchers/VoucherDeleteConfirm.js
@@ -2,10 +2,12 @@ import React from 'react';
 import autoBind from 'react-autobind';
 import cookie from 'react-cookie';
 import { Button, Modal } from 'react-bootstrap';
-import 'react-select2-wrapper/css/select2.css';
-
+import SweetAlert from 'sweetalert-react';
+import axios from 'axios';
 
 import backend from '../../configs/backend';
+
+import 'sweetalert/dist/sweetalert.css';
 
 class VoucherDeleteConfirm extends React.Component {
     constructor(props) {
@@ -13,67 +15,95 @@ class VoucherDeleteConfirm extends React.Component {
         autoBind(this);
 
         this.state = {
-            loading: false,
-            errors: ''
+            voucher: {},
+            swal: {
+                show: false,
+                title: '',
+                message: '',
+                type: 'info',
+                confirm_button: false,
+                cancel_button: false,
+            },
         };
 
     }
 
-    close() {
-        this.setState({ showModal: false });
-    }
-    open() {
-        this.setState({ showModal: true });
-    }
-
-
     componentWillMount() {
 
     }
-    _create() {
+
+    requestDelete() {
+        console.log('deleting');
+        this.setState({
+            swal: {
+                show: true,
+                title: 'Deleting ...',
+                text: 'We are deleting your data',
+                type: 'info',
+                confirm_button: false,
+                cancel_button : false
+            }
+        });
         var token = cookie.load('token');
-        return $.ajax({
-            url: backend.url + '/api/voucher/' + this.props.id,
-            type: 'DELETE',
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', 'Bearer ' + token);
-                this.setState({ loading: true });
-            }.bind(this)
+        axios.create({
+            baseURL: backend.url,
+            timeout: 1000,
+            headers: {
+                Accept: 'application/json',
+                Authorization: 'Bearer ' + token
+            }
+        }).delete('/api/vouchers/' + this.props.id)
+        .then(response => {
+            let swal = this.state.swal;
+            if (response.data.error) {
+                swal.title = 'Gagal';
+                swal.type = 'error';
+            } else {
+                swal.title = 'Berhasil';
+                swal.type = 'success';
+                swal.confirm_button = true;
+            }
+            swal.text = response.data.message;
+            this.setState({ swal: swal });
+            location.reload();
+        }).catch(error => {
+            let swal = this.state.swal;
+
+            swal.confirm_button = true;
+            swal.title = 'Gagal';
+            swal.type = 'error';
+            swal.text = 'Please check your connection';
+            this.setState({ swal: swal });
+            console.log(error);
         });
     }
-    deleteNow() {
 
-        var xhr = this._create();
-        xhr.done(this._onSuccess)
-            .fail(this._onError)
-            .always(this.hideLoading);
+    showSwal() {
+        let swal = this.state.swal;
+        swal.show = true;
+        swal.title = 'Are You Sure?';
+        swal.text = 'You will not be able to recover this voucher';
+        swal.type = 'warning';
+        swal.cancel_button = true;
+        swal.confirm_button = true;
+        this.setState({
+            swal: swal
+        });
     }
-    _onSuccess(data) {
-        console.log(data);
-        console.log('success delete');
-        location.reload();
+
+    dismissSwal() {
+        let swal = this.state.swal;
+        swal.show = false;
+        this.setState({swal : swal});
     }
-    _onError(data) {
-        console.log(data);
-        console.log('error');
-        var message = 'Failed to login';
-        var res = data.responseJSON;
-        if (res.message) {
-            message = data.responseJSON.message;
-        }
-        if (res.errors) {
-            this.setState({
-                errors: res.errors
-            });
-        }
-    }
+
     render() {
 
         return (
             <div className="row">
                 <div className="col-sm-12">
                     <a className="pull-right"
-                        onClick={this.open} >
+                        onClick={this.showSwal} >
                         <i className="fa fa-trash"></i></a>
                 </div>
                 <Modal bsSize="small" aria-labelledby="contained-modal-title-lg" show={this.state.showModal} onHide={this.close}>
@@ -87,12 +117,23 @@ class VoucherDeleteConfirm extends React.Component {
                         </div>
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button onClick={this.deleteNow} bsStyle="danger">Delete</Button>
+                        <Button onClick={this.showSwal} bsStyle="danger">Delete</Button>
                         <Button onClick={this.close}>Close</Button>
                     </Modal.Footer>
                 </Modal>
+                <SweetAlert
+                    show={this.state.swal.show}
+                    title={this.state.swal.title}
+                    text={this.state.swal.text}
+                    type={this.state.swal.type}
+                    showConfirmButton={this.state.swal.confirm_button}
+                    showCancelButton={this.state.swal.cancel_button}
+                    confirmButtonColor="#DD6B55"
+                    onConfirm={this.requestDelete}
+                    onCancel={this.dismissSwal}
+                    />
             </div>
-        )
+        );
     }
 }
 
