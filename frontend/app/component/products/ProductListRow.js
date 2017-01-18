cab10767dd1ca182a9599c55dcab7ecce0dd0961
andangr <dms2707@gmail.com>
2017-01-18 05:51:05 -0500

    bug fix CRUD Product

--- frontend/app/component/products/ProductListRow.js
+++ frontend/app/component/products/ProductListRow.js
@@ -1,39 +1,39 @@
 import React from 'react';
 import autoBind from 'react-autobind';
-import {ButtonToolbar, OverlayTrigger, Button, Popover} from 'react-bootstrap';
+import { ButtonToolbar, OverlayTrigger, Button, Popover } from 'react-bootstrap';
 
 import DeleteProductConfirm from './DeleteProductConfirm';
 import EditProduct from './EditProduct';
 
 class ProductListRow extends React.Component {
-	constructor(props){
+    constructor(props) {
         super(props);
         autoBind(this);
     }
-    edit(id){
+    edit(id) {
         console.log("deleteted")
     }
-    deleteProduct(id){
-         
+    deleteProduct(id) {
+
     }
 
-	render (){
-		return (
+    render() {
+        return (
             <tr>
-                
+
                 <td>{this.props.details.id}</td>
                 <td>{this.props.details.product_name}</td>
                 <td>{this.props.details.description}</td>
                 <td>{this.props.details.category_id}</td>
                 <td>{this.props.details.sub_category_id}</td>
                 <td>{this.props.details.price}</td>
-                <td>{ (this.props.details.is_active == true) ? 'Active' : 'Inactive' }</td>
+                <td>{(this.props.details.is_active == true) ? 'Active' : 'Inactive'}</td>
                 <td>
-                    <EditProduct id={this.props.details.id} details={this.props.details}/><DeleteProductConfirm id={this.props.details.id}/>
+                    <EditProduct id={this.props.details.id} details={this.props.details} /><DeleteProductConfirm id={this.props.details.id} />
                 </td>
             </tr>
-		)
-	}
+        )
+    }
 };
 /*<button className="btn btn-danger btn-sm pull-right" 
                         onClick={this.deleteProduct(this.props.details.id)} >
